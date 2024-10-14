b54b100e7a3ab1191b13425ce33f8ea34bd9daf3
Prajna Paramita Satapathy <22054110@kiit.ac.in>
2024-10-14 08:52:36 +0530

    Add files via upload

---
+++ cl1.175stackintro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+	stack<int> s;
+	s.push(2);
+	s.push(3);
+	s.pop();
+	cout<<s.top()<<endl;
+	if(s.empty())
+		cout<<"Stack is empty"<<endl;
+	else
+		cout<<"Stack is not empty"<<endl;
+	return 0;
+}
+
